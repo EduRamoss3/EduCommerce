8bd34729eed0b1506ce2894532eb3e4e143a8871
Eduardo Ramos <eduardoramossilva@live.com>
2024-03-23 16:57:07 -0300

    adicionando upload Files ao sistema

--- WebApplication2/Areas/Admin/Controllers/AdminImagensController.cs
+++ WebApplication2/Areas/Admin/Controllers/AdminImagensController.cs
@@ -23,5 +23,36 @@ namespace EduCommerceWeb.Areas.Admin.Controllers
 
             return View();
         }
+        public async Task<IActionResult> UploadFiles(List<IFormFile> files)
+        {
+            if (files == null || files.Count == 0)
+            {
+                ViewData["Erro"] = "Erro: Arquivo(s) não selecionado(s)";
+                return View(ViewData);
+            }
+            if (files.Count > 10)
+            {
+                ViewData["Erro"] = "Erro: Quantidade de arquivos excedeu o limite (10)";
+                return View(ViewData);
+            }
+            long size = files.Sum(f => f.Length);
+            var filePathsName = new List<string>();
+            var filePath = Path.Combine(_hostingEnvironment.WebRootPath, _myConfig.NomePastaImagensProdutos);
+            foreach (var formFile in files)
+            {
+                if (formFile.FileName.Contains(".jpg") || formFile.FileName.Contains(".gif") || formFile.FileName.Contains(".png"))
+                {
+                    var fileNameWithPath = string.Concat(filePath, "\\", formFile.FileName);
+                    filePathsName.Add(fileNameWithPath);
+                    using (var stream = new FileStream(fileNameWithPath, FileMode.Create))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+                }
+            }
+            ViewData["Resultado"] = $"{files.Count} arquivos foram enviados ao servidor, " + $"com tamanho total de: {size} bytes";
+            ViewBag.Arquivos = filePathsName;
+            return View(ViewData);
+        }
     }
 }
